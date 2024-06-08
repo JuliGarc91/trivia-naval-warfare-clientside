ba4c29556ef22ebe367aa1356a15a821c4047aa9
juligarc91 <julissagarcia@pursuit.org>
2024-06-08 13:32:05 -0400

    added browser routed and added route for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
 
 const App = () => {
   return <div>
     <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
     </h1>
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+    </Routes>
   </div>;
 };
 
