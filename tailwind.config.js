55b4b014458853de36fe4b9e853c3f6626ea5a8a
juligarc91 <julissagarcia@pursuit.org>
2024-06-08 13:34:08 -0400

    added custome colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wine':'#593943', // sunken ship color
+        'dark-red':'#815e69', // can be used - not sure where
+        'hit-red':'#b72d51', // for hits
+        // --- main colors ---
+        'forest':'#3a5139', // battlefield
+        'forest-btn':'#2a3e2a',
+        'light-navy':'#54677c', // battlefield
+        'navy':'#2f4255' // battlefield
+      },
+    },
   },
   plugins: [],
 }
