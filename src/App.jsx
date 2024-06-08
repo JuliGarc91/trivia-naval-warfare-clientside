8aa38e97cc201afb1c60911ce2a19f231185ee62
Jaden <jadenglenn@pursuit.org>
2024-06-08 14:07:28 -0400

    Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "./components/Landing";
+import HowTo from "./components/Howto";
 
 const App = () => {
+
+  
+  const NavBarWrapper = () => {
+    const location = useLocation()
+    return location.pathname !== "/" && <Nav />
+  }
+
+
   return <div>
     <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
     </h1>
     <Routes>
       <Route path="/" element={<Landing/>}/>
+      <Route path="/howto" element={<HowTo />}/>
     </Routes>
-  </div>;
+  </div>
 };
 
 export default App;
