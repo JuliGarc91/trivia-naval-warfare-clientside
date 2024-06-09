bef82b98140413c355c552a7245cd34fb04c6588
juligarc91 <julissagarcia@pursuit.org>
2024-06-09 16:17:33 -0400

    addedstyles

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,15 +13,15 @@ const Landing = () => {
   }
   return (
     <div className="landing-container bg-black h-screen md:h-auto">
-      <h1 className='text-3xl font-bold text-white text-center  mb-14 p-6'>TNW</h1>
+      <h1 className='text-6xl font-bold text-white text-center  mb-10 p-6' style={{ fontFamily: "Black Ops One, system-ui", fontStyle: 'normal' }}>TNW</h1>
         <div className='landing-img-container p-4'>
           <img className='landing-img w-full aspect-auto border rounded-lg ' src= "https://res.cloudinary.com/dhx9ogsz5/image/upload/c_crop,ar_16:9/v1717771875/A_military_submarine_approaching_a_war_scene_lzcqbw.jpg"
           alt="img-of-sailor-at-sea"
           />
           <button
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-forest hover:bg-forest-btn text-white py-2 px-4 rounded"
+          className="p-2 text-6xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-wine/90 hover:bg-forest-btn text-white py-2 px-4 rounded"
           onClick={() => handleClick()}
-          >Play Game Here</button>
+          style={{ fontFamily: "Black Ops One, system-ui", fontStyle: 'normal' }}>Play Game Here</button>
           <span className=' text-yellow-400' style={{ fontFamily: "Kode Mono, monospace", fontStyle: 'normal' }}>Enter the Field... If you dare '_'</span>
         </div>
     </div>
