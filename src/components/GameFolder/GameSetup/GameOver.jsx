8e4819f26956ae770642c05e47bd23c3b973ea34
juligarc91 <julissagarcia@pursuit.org>
2024-06-09 13:55:17 -0400

    styled game

--- src/components/GameFolder/GameSetup/GameOver.jsx
+++ src/components/GameFolder/GameSetup/GameOver.jsx
@@ -3,11 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 
 const GameOver = ({ winner }) => {
   return (
-    <div>
-      <h1>GameOver</h1>
+    <div className="flex flex-col justify-center items-center bg-zinc-700/80 p-8 border-4 border-black">
+      <h1 className="text-center text-6xl">Game Over</h1>
       <p>{`Congratulations ${winner} you win!`}</p>
+      <br/>
       <Link to="/">
-        <button className="border-2 border-black p-2">Back to Home</button>
+        <button className="bg-navy border-2 border-black p-2 hover:bg-light-navy">Back to Home</button>
       </Link>
     </div>
   );
