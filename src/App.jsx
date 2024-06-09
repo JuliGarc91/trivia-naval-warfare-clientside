956190edddbb3b273fd0f9418b2dde374c877a32
Jaden <jadenglenn@pursuit.org>
2024-06-08 22:39:26 -0400

    nav & howto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "./components/Landing";
 import About_Mission from "./components/About_Mission";
 import HowTo from "./components/Howto";
+import Nav from "./components/Nav"
 
 const App = () => {
 
@@ -14,16 +15,16 @@ const App = () => {
 
 
   return (
+  <>
+      <NavBarWrapper />
     <div>
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-    <Routes>
+     <Routes>
       <Route path="/" element={<Landing/>}/>
       <Route path="/howto" element={<HowTo />}/>
       <Route path="/mission_&_about" element={<About_Mission />}/>
-    </Routes>
-  </div>
+     </Routes>
+     </div>
+  </>
   )
 };
 
